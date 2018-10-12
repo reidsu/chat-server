d8f999beda5cde2b9db4d4d4cc5b12e32109ddb6
unknown <suhe@hikvision.com>
2018-10-12 14:37:13 +0800

    完成了基本消息服务

--- src/controller/messageProvider.js
+++ src/controller/messageProvider.js
@@ -1,18 +1,22 @@
 import store from "../storage/index";
 
-class MessageProvider {
+export class MessageProvider {
+  init(io) {
+    this.io = io;
+  }
   getAll() {
     return store.getList();
   }
   sendMessage(text) {
     const message = store.addMessage(text);
-    this._dispatch(message);
+    this._dispatch();
   }
   removeAll() {
     store.removeAll();
+    this._dispatch();
   }
   _dispatch() {
-
+    this.io.broadcast('message', "hello, world");
   }
 }
 
--- src/server.js
+++ src/server.js
@@ -1,10 +1,29 @@
 import Koa from "koa";
 import router from "./router/index";
 import bodyParser from "koa-bodyparser";
+import IO from "koa-socket";
+import cors from "koa2-cors";
+import msgPro from "./controller/messageProvider";
 
 const app = new Koa();
+const io = new IO();
 
+
+io.attach( app )
+
+app._io.on( 'connection', sock => {
+  sock.join("chat");
+})
+
+// init 
+msgPro.init(io);
+// setTimeout(() => {
+//   io.broadcast('message', "hello, world");
+
+// }, 5000);
+app.use(cors());
 app.use(bodyParser());
 app.use(router.routes());
+
 
 app.listen(3001);
