f6c343777187f9f18ab73967a7fed5a40225c5e8
unknown <suhe@hikvision.com>
2018-10-12 16:32:00 +0800

    重新打包

--- dist/storage/cacheStore.js
+++ dist/storage/cacheStore.js
@@ -6,12 +6,6 @@ Object.defineProperty(exports, "__esModule", {
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var _message = require("./message");
-
-var _message2 = _interopRequireDefault(_message);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var Store = function () {
@@ -29,7 +23,7 @@ var Store = function () {
   }, {
     key: "addMessage",
     value: function addMessage(text) {
-      var message = new _message2.default(text);
+      var message = new Message(text);
       this.messageList.push(message);
       return message;
     }
